d48682770b1ab16d66dac3cbdd4589476ba47f3f
DEEPTHI VM <118994474+DEEPTHIVM@users.noreply.github.com>
2022-12-06 09:54:30 -0800

    Add files via upload

---
+++ TEA.py
@@ -0,0 +1,21 @@
+#PROGRAMME FOR BILLING SYSTEM OF A TEA SHOP
+
+a=7
+
+b=8
+
+c=10
+
+tea=int(input('tea:'))
+
+tea=tea*7
+
+coffee=int(input('coffee:'))
+
+coffee=coffee*8
+
+vada=int(input('vada:'))
+
+vada=vada*10
+
+print(tea+coffee+vada)
