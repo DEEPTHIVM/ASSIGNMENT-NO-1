b50e47512ce81fcb9d38f823f6e7c4e1d3878c76
DEEPTHI VM <118994474+DEEPTHIVM@users.noreply.github.com>
2022-12-06 10:12:24 -0800

    Add files via upload

---
+++ zod.py
@@ -0,0 +1,27 @@
+#programme to determine zodiac sign
+
+x= input('ENTER YOUR MONTH OF BIRTH:')
+
+if x == 'january' :print('CAPRICON')
+
+elif x == 'february' :print('AQUARIUS')
+
+elif x == 'march' :print('PISCES')
+
+elif x == 'april' :print('ARIES')
+
+elif x == 'may' :print('TAURUS')
+
+elif x == 'june' :print('GEMINI')
+
+elif x == 'july' :print('CANCER')
+
+elif x == 'august' :print('LEO')
+
+elif x == 'september' :print('VIRGO')
+
+elif x == 'october' :print('LIBRA')
+
+elif x == 'november' :print('SCORPIO')
+
+elif x == 'december' :print('SAGITTARIUS')
