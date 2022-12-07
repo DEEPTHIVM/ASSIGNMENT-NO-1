1778fa81ae651b4c860a9837810ceecd3ca78180
DEEPTHI VM <118994474+DEEPTHIVM@users.noreply.github.com>
2022-12-06 21:47:27 -0800

    Add files via upload

---
+++ MATRIMONY.py
@@ -0,0 +1,15 @@
+#FRONT PAGE OF MATRIONY SITE
+
+print('WELCOME TO KALYANI MATRIMONY')
+
+input('name:')
+
+input('gender:')
+
+int(input('age:'))
+
+input('job:')
+
+input('adress:')
+
+print('YOU ARE IN')
